498a7b1b42a672d1be4a5a05f499826c2b800b3c
Igor Smurov <smurov_igor@mail.ru>
2018-12-17 22:42:29 +0300

    add init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
+# Comands for fast servers config
+
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
