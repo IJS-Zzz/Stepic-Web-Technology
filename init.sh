6d54fe2da084f65e9da14d2bf0aaffe8e6bc9740
Igor Smurov <smurov_igor@mail.ru>
2018-12-18 13:25:58 +0000

    static files - done

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 # Comands for fast servers config
 
 # Nginx
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 # Gunicorn
