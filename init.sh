5cd974f625095d07c33884735e2d82894971b00f
Igor Smurov <smurov_igor@mail.ru>
2018-12-18 06:38:39 +0000

    update

--- init.sh
+++ init.sh
@@ -1,7 +1,13 @@
 # Comands for fast servers config
 
+# Nginx
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
+# Gunicorn
 # sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
+
+# MySQL
 sudo /etc/init.d/mysql start
