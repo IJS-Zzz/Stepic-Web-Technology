735070b2cb74ac35df01eeab59a5b0b30336f6ea
Igor Smurov <smurov_igor@mail.ru>
2018-12-19 12:08:20 +0300

    new config for gunicorn

--- init.sh
+++ init.sh
@@ -1,13 +1,18 @@
 # Comands for fast servers config
 
 # Nginx
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
-# Gunicorn
-sudo ln -sf /home/box/web/etc/gunicorn_config.py  /etc/gunicorn.d/config.py
+# Correct way to config Gunicorn app
+sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
 sudo /etc/init.d/gunicorn restart
+
+# Gunicorn
+# sudo ln -sf /home/box/web/etc/gunicorn_config.py  /etc/gunicorn.d/config.py
+# sudo /etc/init.d/gunicorn restart
 # Run Gunicorn in shell for start with config file
 #--> gunicorn -c /etc/gunicorn.d/config.py hello:app
 
