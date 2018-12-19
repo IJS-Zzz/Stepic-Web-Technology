ea5248205dee9712fd3ce7f28edf8a6cb0c528ce
Igor Smurov <smurov_igor@mail.ru>
2018-12-19 20:07:50 +0300

    update urls

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -22,10 +22,10 @@ from qa.views import test
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
     url(r'^$', test),
-    url(r'^login/', test),
-    url(r'^signup/', test),
+    url(r'^login/$', test),
+    url(r'^signup/$', test),
     url(r'^question/', include('qa.urls')),
-    url(r'^ask/', test),
-    url(r'^popular/', test),
-    url(r'^new/', test),
+    url(r'^ask/$', test),
+    url(r'^popular/$', test),
+    url(r'^new/$', test),
 ]
