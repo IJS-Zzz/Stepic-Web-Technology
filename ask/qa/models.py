17c175be161ca52d07ec5663f9ad7aebb719d01d
Igor Smurov <smurov_igor@mail.ru>
2018-12-21 00:02:08 +0300

    add models in qa.models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -32,27 +32,3 @@ class Answer(models.Model):
     added_at = models.DateField(auto_now_add=True)  #, blank=True)
     question = models.ForeignKey(Question)
     author = models.ForeignKey(User)
-
-"""
-Question - вопрос
-title - заголовок вопроса
-text - полный текст вопроса
-added_at - дата добавления вопроса
-rating - рейтинг вопроса (число)
-author - автор вопроса
-likes - список пользователей, поставивших "лайк"
-
-Answer - ответ
-text - текст ответа
-added_at - дата добавления ответа
-question - вопрос, к которому относится ответ
-author - автор ответа
-
-В качестве модели пользователя - используйте django.contrib.auth.models.User  из стандартной системы авторизации Django.
-
-5) Рядом с моделью Question определите менеджер реализующий следующие методы
-
-QuestionManager - менеджер модели Question
-new - метод возвращающий последние добавленные вопросы
-popular - метод возвращающий вопросы отсортированные по рейтингу
-"""
