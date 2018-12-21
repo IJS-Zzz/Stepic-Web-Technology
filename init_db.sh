91ff6ad5ac8254486ad07bf7c372c1593ba2943b
Igor Smurov <smurov_igor@mail.ru>
2018-12-21 07:07:12 +0300

    add models in qa.models.py

--- init_db.sh
+++ init_db.sh
@@ -1,3 +1,3 @@
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS stepik CHARACTER SET utf8"
 mysql -u root -e "CREATE USER 'django'@'localhost' IDENTIFIED BY 'stepik'"
-mysql -u root -e "GRANT ALL ON stepik.* TO 'django'@'localhost'"
+mysql -u root -e "GRANT ALL ON stepik.* TO 'django'@'localhost' IDENTIFIED BY 'stepik'"
